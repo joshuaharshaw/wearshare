156c07310a90ccebe6d77fc740c616df8ca1c2d1
Dylan Dunne <dylanadunne@gmail.com>
2017-08-22 15:15:30 -0400

    updated server endpoints

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var pg = require('pg');
 var cloudinary = require('cloudinary');
-var pool = require("./pgconnectionpool");
+var pool = require("./pg-connection-pool");
 
 cloudinary.config({
 	cloud_name:"dr1gz6f3y",
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.get('/users', function(req,res){
-	pool.query('SELECT * FROM wearshare.users').then(function(result){
+	pool.query('SELECT * FROM users').then(function(result){
 		res.send(result.rows);
 	}).catch(function(err){
 		console.log(err);
@@ -26,7 +26,7 @@ app.get('/users', function(req,res){
 });
 
 app.get('/users/:user_id', function(req,res){
-	var sql ='SELECT * FROM wearshare.users WHERE user_id=$1::int;' 
+	var sql ='SELECT * FROM users WHERE user_id=$1::int;' 
 	var values = [ req.params.user_id ];
 	pool.query(sql, values).then(function(result){
 		res.status(201).send(result.rows);
@@ -38,7 +38,7 @@ app.get('/users/:user_id', function(req,res){
 });
 
 app.get('users/outfits', function(req,res){
-	pool.query('SELECT * FROM wearshare.outfits').then(function(result){
+	pool.query('SELECT * FROM outfits').then(function(result){
 		res.send(result.rows);
 	}).catch(function(err){
 		console.log(err);
@@ -48,7 +48,7 @@ app.get('users/outfits', function(req,res){
 });
 
 app.get('/users/:user_id/articles', function(req,res){
-	var sql ='SELECT * FROM wearshare.articles WHERE user_id=$1::int;'; 
+	var sql ='SELECT * FROM articles WHERE user_id=$1::int;'; 
 	var values = [ req.params.user_id ];
 	pool.query(sql, values).then(function(result){
 		res.status(201).send(result.rows);
@@ -60,7 +60,7 @@ app.get('/users/:user_id/articles', function(req,res){
 });
 
 app.post('/users', function(req,res){
-	var sql ='INSERT INTO wearshare.users (user_name, password) ' 
+	var sql ='INSERT INTO users (user_name, password) ' 
 		+ 'VALUES ($1::text, $2::text)';
 	var values = [ req.body.user_name, req.body.password ];
 	pool.query(sql, values).then(function(result){
@@ -74,16 +74,15 @@ app.post('/users', function(req,res){
 
 
 app.post('/users/:user_id/articles', function(req, res){
-	var sql ='INSERT INTO wearshare.articles (user_id, path, type) ' 
-		+ 'VALUES ($1::int, $2::text, $3::text)';
-	var values = [ req.params.user_id, req.body.path, req.body.type ];
+	var sql ='INSERT INTO articles (user_id, image_path, article_type, article_desc, article_name) ' 
+		+ 'VALUES ($1::int, $2::text, $3::text, $4::text, $5::text, $6::text)';
+	var values = [ req.params.user_id, req.body.image_path, req.body.article_type, req.body.article_desc, req.body.article_name ];
 	pool.query(sql, values).then(function(result){
 		res.status(201).send("result.rows");
 	}).catch(function(err){
 		console.log(err);
 		res.status(500);
 		res.send("server error");
-		// res.send(req);
 	})
 });
 

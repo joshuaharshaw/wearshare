9abb65bbac69b1d25bf54c4bf2273c6dfb9f5258
Dylan Dunne <dylanadunne@gmail.com>
2017-08-22 15:43:59 -0400

    fixed server

--- public/js/services.js
+++ public/js/services.js
@@ -2,7 +2,7 @@ var app = angular.module("outfitApp");
 
 app.service("profileService" , function ($http) {
 	this.getArticles = function () {
-		return $http.get('/users/2/articles').then(function (response) {
+		return $http.get('/users/1/articles').then(function (response) {
 			return response.data;
 		});
 	};
